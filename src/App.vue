991a4741e984a1282a15e73d447db43cac0b97c3
Roeen <roeenacal@gmail.com>
2023-06-18 08:20:25 -0700

    Add Event component

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Event from './components/Event.vue';
+
+const eventData = [
+  {
+    id: 1,
+    name: "Graduation",
+    details: "wooohoo!!!",
+    date: "2022-09-25",
+    background: "#F34949",
+  },
+  {
+    id: 2,
+    name: "Holidays",
+    details: "wooohoo!!!",
+    date: "2028-10-02",
+    background: "#f9f970",
+  },
+  {
+    id: 3,
+    name: "HolidayYYYY",
+    details: "Get me outta here!",
+    date: "2023-06-18",
+    background: "#7AD3F0",
+  },
+  {
+    id: 4,
+    name: "Birthday",
+    details: "My birthday party",
+    date: "2023-06-17",
+    background: "#F07AEC",
+  },
+  {
+    id: 5,
+    name: "Christmas",
+    details: "ho ho ho",
+    date: "2023-06-19",
+    background: "#EB9A0F",
+  },
+  {
+    id: 6,
+    name: "Conference Talk",
+    details: "dont flop",
+    date: "2026-02-28",
+    background: "#68EE94",
+  },
+];
+
+function daysLeft(event) {
+      const time = Date.parse(event.date) - Date.now();
+      const days = Math.ceil(time / (1000 * 3600 * 24));
+      return days;
+    }
+
+</script>
+
+<template>
+  <ul>
+    <li
+    v-for="event in eventData" 
+		:key="event.id"
+		>
+      <Event 
+      :event="event"
+      :daysLeft="daysLeft(event)"
+      />
+    </li>
+  </ul>
+</template>
+
+<style >
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Event.vue
@@ -0,0 +1,104 @@
+<script setup>
+import { computed } from 'vue';
+
+    
+const props = defineProps({
+	event: {
+		type: Object,
+	},
+	daysLeft: {
+		type: Number
+	}
+}) 
+
+// Function
+// function daysLeftString() {
+// 	const dayOrDays = Math.abs(props.daysLeft) === 1 ? 'day' : 'days' 
+// 	const leftOrAgo = Math.sign(props.daysLeft) != -1 ? 'left' : 'ago'
+// 	return `${dayOrDays} ${leftOrAgo}`
+// }
+
+// Computed
+const daysLeftString = computed(() => {
+	const dayOrDays = Math.abs(props.daysLeft) === 1 ? 'day' : 'days' 
+	const leftOrAgo = Math.sign(props.daysLeft) != -1 ? 'left' : 'ago'
+	return `${dayOrDays} ${leftOrAgo}`
+})
+
+</script>
+
+<template>
+     <article 
+		 v-show=" Math.sign(daysLeft) !== -1 "
+		 :style="{ background: event.background }
+		 ">
+				<div class="data">
+					<h3 class="name">{{ event.name }}!</h3>
+					<p class="details">{{ event.details }}</p>
+				</div>
+				<div class="countdown">
+					<div class="remove_btn_wrapper">
+						<button class="remove_btn">&#10060;</button>
+					</div>
+					<p v-if="daysLeft === 0">Today</p>
+					<p v-else>
+						{{ Math.abs(daysLeft) }}
+						
+						<br />
+						<!-- Function Call -->
+						<!-- <small>{{ daysLeftString() }}</small> -->
+						
+						<!-- Computed Call - this is better for updating data because you don't have to call a function, which requires a browser refresh-->
+						<small>{{ daysLeftString }}</small>
+					</p>
+				</div>
+			</article>
+</template>
+
+
+
+<style lang="scss" scoped>
+article {
+	background: lightslategray;
+  display: flex;
+  align-items: center;
+  border-radius: 1rem;
+  margin: 1rem 0;
+  padding: 0.2rem 1rem;
+  color: whitesmoke;
+  font-weight: 300;
+	justify-content: space-around;
+
+	 .data{
+		flex: 3;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		.name{
+			font-size: 1.8rem;
+		}
+		.details{
+			font-size: 1.6rem;
+		}
+	 }
+	 .countdown {
+		flex: 1;
+		text-align: center;
+		font-size: 1.6rem;
+		border-left: 1px solid;
+
+		.remove_btn_wrapper {
+			text-align: right;
+			.remove_btn{
+			background-color: transparent;
+			border: none;
+			cursor: pointer;
+			
+		}
+		}
+
+	 }
+}
+
+</style>
